b9e9590317ac319b1c3a238b15baf78146302be4
Natalia Stan <popkids.ua@gmail.com>
2025-07-30 14:26:50 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import json from "@rollup/plugin-json";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), json()],
+  base: "/",
   build: {
     sourcemap: true,
   },
